ce188c46a730346288b363978cf251bf2c24277a
João Gameiro <jmfgameiro@gmail.com>
2016-08-08 17:22:39 +0100

    Generic Samples

--- src/main/resources/js/app.js
+++ src/main/resources/js/app.js
@@ -1,6 +1,28 @@
+var samples = [ "helloworld" ];
 
+$(window).load(function(){
+    // CREATE SAMPLES
+    var samplesDiv = $('#samplesDiv');
+    $.each( samples, function(index, value){
+        var sampleDiv = document.createElement('div');
+        sampleDiv.setAttribute('class','row');
+        var sampleLink = document.createElement('a');
+        sampleLink.setAttribute('href','samples/'+value+'/index.html');
+        sampleLink.setAttribute('class','col-xs-12 col-md-4 mockup-container');
+        var sampleImg = document.createElement('img');
+        sampleImg.setAttribute('id','sample'+value);
+        sampleLink.appendChild(sampleImg);
+        sampleDiv.appendChild(sampleLink);
+        samplesDiv.append(sampleDiv);
+    });
+
+});
 
 $(document).ready(function(){
-    alert("HELLO WORLD!");
+
+    $('.row > a').each(function() {
+        $(this).att('height', $(this).width() + 'px');
+    });
+
 });
 
