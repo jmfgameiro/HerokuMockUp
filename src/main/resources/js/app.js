2528adde30f88b965a19544a3e9ff17f36a63f74
João Gameiro <jmfgameiro@gmail.com>
2016-08-08 17:48:39 +0100

    Multiple Samples Test

--- src/main/resources/js/app.js
+++ src/main/resources/js/app.js
@@ -1,20 +1,18 @@
-var samples = [ "helloworld" ];
+var samples = [ "helloworld", "singlepage" ];
 
 $(document).ready(function(){
 
     // CREATE SAMPLES
     var samplesDiv = $('#samplesDiv');
     $.each( samples, function(index, value){
-        var sampleDiv = document.createElement('div');
-        sampleDiv.setAttribute('class','row');
+        // Create link
         var sampleLink = document.createElement('a');
         sampleLink.setAttribute('href','samples/'+value+'/index.html');
         sampleLink.setAttribute('class','col-xs-12 col-md-4 mockup-container');
         var sampleImg = document.createElement('img');
         sampleImg.setAttribute('id','sample'+value);
         sampleLink.appendChild(sampleImg);
-        sampleDiv.appendChild(sampleLink);
-        samplesDiv.append(sampleDiv);
+        samplesDiv.append(sampleLink);
     });
 
 });
