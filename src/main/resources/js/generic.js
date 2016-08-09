b7b9ac2e58924f6e347e50b21954ff23d9ddfa3d
João Gameiro <jmfgameiro@gmail.com>
2016-08-09 10:04:39 +0100

    Snapshot Correction Test

--- src/main/resources/js/generic.js
+++ src/main/resources/js/generic.js
@@ -4,17 +4,13 @@ $(function() {
     $("#navbarIncluded").load("/navbar.html");
 });
 
-$(document).ready(function(){
-
-    $("#navbarSnapshot").click(function(){
-        alert("Button Click");
-        html2canvas(document.body, {
-            onrendered: function(canvas) {
-                var myImage = canvas.toDataURL("image/png");
-                window.open(myImage);
-            }
-        });
+$("#navbarSnapshot").click(function(){
+    alert("Button Click");
+    html2canvas(document.body, {
+        onrendered: function(canvas) {
+            var myImage = canvas.toDataURL("image/png");
+            window.open(myImage);
+        }
     });
-
 });
 
