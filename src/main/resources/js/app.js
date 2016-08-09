f180dc0618590ab1915d8914a3bc098f6b24e2f7
João Gameiro <jmfgameiro@gmail.com>
2016-08-09 09:57:36 +0100

    html2canvas test

--- src/main/resources/js/app.js
+++ src/main/resources/js/app.js
@@ -1,16 +1,16 @@
-var samples = [ "helloworld", "singlepage" ];
+var samples = [ "singlepagevertical" ];
 
 $(document).ready(function(){
 
     // CREATE SAMPLES
-    var samplesDiv = $('#samplesDiv');
+    var samplesDiv = $("#samplesDiv");
     $.each( samples, function(index, value){
         // Create link
-        var sampleLink = document.createElement('a');
-        sampleLink.setAttribute('href','samples/'+value+'/index.html');
-        sampleLink.setAttribute('class','col-xs-12 col-md-4 mockup-container');
-        var sampleImg = document.createElement('img');
-        sampleImg.setAttribute('id','sample'+value);
+        var sampleLink = document.createElement("a");
+        sampleLink.setAttribute("href","samples/"+value+"/index.html");
+        sampleLink.setAttribute("class","col-xs-12 col-md-4 mockup-container");
+        var sampleImg = document.createElement("img");
+        sampleImg.setAttribute("id","sample"+value);
         sampleLink.appendChild(sampleImg);
         samplesDiv.append(sampleLink);
     });
@@ -19,12 +19,10 @@ $(document).ready(function(){
 
 $(window).load(function(){
 
-    $('#samplesDiv > a').each(function() {
+    $("#samplesDiv > a").each(function() {
         $(this).height($(this).width());
     });
 
 });
-
-
 
 
---
+++ src/main/resources/js/generic.js
@@ -0,0 +1,20 @@
+//$("#footerYear").text(new Date().getFullYear());
+
+$(function() {
+    $("#navbarIncluded").load("/html/navbar.html");
+});
+
+$(document).ready(function(){
+
+    $("#navbarSnapshot").click(function(){
+        alert("Button Click");
+        html2canvas(document.body, {
+            onrendered: function(canvas) {
+                var myImage = canvas.toDataURL("image/png");
+                window.open(myImage);
+            }
+        });
+    });
+
+});
+
